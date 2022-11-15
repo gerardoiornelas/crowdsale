c5cf3a6e1d884554873715705b615740cb534900
Gerardo Ornelas <gerardo@ornelastechnologies.com>
2022-11-15 06:47:13 -0600

    init crowdsale

---
+++ contracts/Crowdsale.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./Token.sol";
+
+contract Crowdsale {
+    Token public token;
+
+    constructor(Token _token) {
+        token = _token;
+    }
+
+    function buyTokens(uint256 _amount) public {
+        token.transfer(msg.sender, _amount);
+    }
+}
