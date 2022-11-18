e2ffbfe8737a8ed4a0d5d1738a0f6f0fc5ccf69f
Gerardo Ornelas <gerardo@ornelastechnologies.com>
2022-11-17 21:22:28 -0600

    git contracts

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -4,13 +4,57 @@ pragma solidity ^0.8.0;
 import "./Token.sol";
 
 contract Crowdsale {
+    address public owner;
     Token public token;
+    uint256 public price;
+    uint256 public tokensSold;
+    uint256 public maxTokens;
 
-    constructor(Token _token) {
+    event Buy(uint256 amount, address buyer);
+    event Finalize(uint256 tokensSold, uint256 ethRaised);
+
+    constructor(
+        Token _token,
+        uint256 _price,
+        uint256 _maxTokens
+    ) {
+        owner = msg.sender;
         token = _token;
+        price = _price;
+        maxTokens = _maxTokens;
     }
 
-    function buyTokens(uint256 _amount) public {
-        token.transfer(msg.sender, _amount);
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
+    }
+
+    receive() external payable {
+        uint256 amount = msg.value / price;
+        buyTokens(amount * 1e18);
+    }
+
+    function buyTokens(uint256 _amount) public payable {
+        require(msg.value >= (_amount / 1e18) * price);
+        require(token.balanceOf(address(this)) >= _amount);
+        require(token.transfer(msg.sender, _amount));
+
+        tokensSold = tokensSold + _amount;
+        emit Buy(_amount, msg.sender);
+    }
+
+    function setPrice(uint256 _price) public onlyOwner {
+        price = _price;
+    }
+
+    function finalize() public onlyOwner {
+        uint256 remainingTokens = token.balanceOf(address(this));
+        require(token.transfer(owner, remainingTokens));
+
+        uint256 value = address(this).balance;
+        (bool sent, ) = owner.call{value: value}("");
+        require(sent);
+
+        emit Finalize(tokensSold, value);
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,20 +7,34 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // Deployment goes here
+  const NAME = "Dapp University";
+  const SYMBOL = "DAPP";
+  const MAX_SUPPLY = "1000000";
+  const PRICE = ethers.utils.parseUnits("0.025", "ether");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const Token = await hre.ethers.getContractFactory("Token");
+  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await token.deployed();
+  console.log(`Token deployed to: ${token.address}\n`);
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const Crowdsale = await hre.ethers.getContractFactory("Crowdsale");
+  const crowdsale = await Crowdsale.deploy(
+    token.address,
+    PRICE,
+    ethers.utils.parseUnits(MAX_SUPPLY)
   );
+  await crowdsale.deployed();
+  console.log(`Crowdsale deployed to: ${crowdsale.address}\n`);
+
+  // Send tokens to crowdsale
+  const transaction = await token.transfer(
+    crowdsale.address,
+    ethers.utils.parseUnits(MAX_SUPPLY, "ether")
+  );
+  await transaction.wait();
+  console.log(`Tokens transfered to crowdsale`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
