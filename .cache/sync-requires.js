a0f450ff4b17c1e89c5ca3ce96c09bec9ed5ee66
Gerardo Ornelas <gerardo@ornelastechnologies.com>
2023-02-13 16:40:48 -0600

    update config

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/ornelastechnologies/Documents/GitHub/crowdsale/.cache/dev-404-page.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/ornelastechnologies/Documents/GitHub/crowdsale/src/pages/index.js")),
   "component---src-pages-privacy-policy-js": preferDefault(require("/Users/ornelastechnologies/Documents/GitHub/crowdsale/src/pages/privacy-policy.js")),
   "component---src-pages-thanks-js": preferDefault(require("/Users/ornelastechnologies/Documents/GitHub/crowdsale/src/pages/thanks.js"))
